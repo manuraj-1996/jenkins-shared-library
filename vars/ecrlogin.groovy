480edaac98ad765320db4cace3861ac07ad22fd0
manuraj-1996 <manojgopalraj1996@gmail.com>
2026-04-16 14:38:59 +0530

    Update ecrlogin.groovy

--- vars/ecrlogin.groovy
+++ vars/ecrlogin.groovy
@@ -1,9 +1,7 @@
-def call(Map config = [:]) {
-    def region = config.region ?: env.AWS_REGION
-    def registryUrl = "https://${config.accountId ?: env.ACCOUNT_ID}.dkr.ecr.${region}.amazonaws.com"
-    
-    // This plugin-based method handles the token exchange automatically
-    docker.withRegistry(registryUrl, "ecr:${region}:${config.credentialsId ?: 'aws-cred'}") {
-        echo "Successfully logged into ECR"
-    }
+// vars/projectconfig.groovy
+def call() {
+    return [
+        ecr_url: '772064137213.dkr.ecr.us-east-1.amazonaws.com/devopsproject',
+        aws_creds: 'aws-cred' // <--- MUST match the ID in Jenkins Credentials
+    ]
 }
